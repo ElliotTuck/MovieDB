0e92c7ae202ea8e45036000126290e83a3f6b857
Elliot Tuck <eat3774@gmail.com>
2018-11-23 22:48:37 -0500

    Add movie entry endpoint
    Add new form and endpoint for entering movie information into the
    database through the application.

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,25 @@
-from wtforms import Form, StringField, validators
+from wtforms import Form, StringField, IntegerField, TextAreaField,\
+    RadioField, SelectMultipleField, validators
+from wtforms.fields.html5 import DateField
 
 class SearchForm(Form):
     searchbar = StringField('Search Movies', [validators.DataRequired()])
+
+class MovieEntryForm(Form):
+    name = StringField('Name', [validators.DataRequired()])
+    release_date = DateField('Release Date', [validators.Optional()])
+    duration = IntegerField('Duration (minutes)', [validators.Optional()])
+    description = TextAreaField('Description (max 256 characters)',
+                                [validators.Length(max=256),
+                                 validators.Optional()])
+    budget = IntegerField('Budget', [validators.Optional()])
+    mpaa_rating = RadioField('MPAA Rating', [validators.Optional()],
+                             choices=[('G','G'), ('PG','PG'),
+                                      ('PG-13','PG-13'), ('R','R'),
+                                      ('NC-17','NC-17')])
+    genres = SelectMultipleField('Genre', [validators.Optional()],
+                                 choices=[('Horror','Horror'),
+                                          ('Comedy','Comedy'),
+                                          ('Drama','Drama'),
+                                          ('Action','Action'),
+                                          ('Thriller','Thriller')])
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from sqlalchemy import create_engine
 from flask_login import login_user, logout_user, current_user, login_required,\
     LoginManager
-from forms import SearchForm
+from forms import SearchForm, MovieEntryForm
 
 app = Flask(__name__)
 engine = create_engine('postgresql://Elliot@localhost:5432/moviedb')
@@ -22,6 +22,30 @@ def main():
         titles = []
         for row in result:
             titles.append(row['name'].strip())
-        print(titles)
         return render_template('search_results.html', titles=titles)
     return render_template("index.html", form=form)
+
+@app.route('/enter_movie', methods=['GET', 'POST'])
+def enter_movie():
+    form = MovieEntryForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        release_date = form.release_date.data if form.release_date.data \
+            else 'NULL'
+        duration = form.duration.data if form.duration.data else 'NULL'
+        description = form.description.data
+        budget = form.budget.data if form.budget.data else 'NULL'
+        mpaa_rating = form.mpaa_rating.data if form.mpaa_rating.data \
+            else 'NULL'
+        genres = form.genres.data
+        connection = engine.connect()
+        result = connection.execute('SELECT COUNT(*) FROM Movie')
+        num_movies = result.fetchone()[0]
+        id_val = int(num_movies) + 1
+        connection.execute("INSERT INTO Movie(Id, Name, ReleaseDate,"   \
+                           "Duration, Description, Budget, MPAARating)" \
+                           "VALUES ({}, '{}', '{}', {}, '{}', {}, '{}')".format
+                           (id_val, name, release_date, duration, description,
+                            budget, mpaa_rating))
+        return 'submitted successfully'
+    return render_template('enter_movie.html', form=form)
